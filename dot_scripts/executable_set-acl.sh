dd44d3593e0a8d81ee4bde317e0610502d7f05d3
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-06-28 17:43:37 -0600

    Update executable_set-acl.sh

--- dot_scripts/executable_set-acl.sh
+++ dot_scripts/executable_set-acl.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
-setfacl setfacl -PRdm u::rwX,g::rwX,o::r $PWD \
-    && setfacl -PRm u::rwX,g::rwX,o::r $PWD
+setfacl -PRdm u::rwX,g::rwX,o::r $PWD && setfacl -PRm u::rwX,g::rwX,o::r $PWD
