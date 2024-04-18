02ae3f2ff0e120de082650c0455667cb0769fe7e
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2024-04-18 15:50:19 -0600

    :fire: remove p10k plugins

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -44,15 +44,15 @@
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # status                # exit code of the last command
     # command_execution_time# duration of the last command
-    background_jobs         # presence of background jobs
+    # background_jobs       # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     # asdf                  # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     # anaconda              # conda environment (https://conda.io/)
-    pyenv                   # python environment (https://github.com/pyenv/pyenv)
+    # pyenv                 # python environment (https://github.com/pyenv/pyenv)
     # goenv                 # go environment (https://github.com/syndbg/goenv)
     # nodenv                # node.js version from nodenv (https://github.com/nodenv/nodenv)
-    nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
+    # nvm                   # node.js version from nvm (https://github.com/nvm-sh/nvm)
     # nodeenv               # node.js environment (https://github.com/ekalinin/nodeenv)
     # node_version          # node.js version
     # go_version            # go version (https://golang.org)
@@ -107,7 +107,7 @@
     # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
-    chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
+    # chezmoi_shell         # chezmoi shell (https://www.chezmoi.io/)
   )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
