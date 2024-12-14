8f8b0b63e160bb54e7555432b4545d7100064da2
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2024-12-14 16:06:18 -0600

    :bug: use installed cdk

--- dot_scripts/executable_cdkgc.sh
+++ dot_scripts/executable_cdkgc.sh
@@ -3,5 +3,5 @@
 REGION=$(aws configure get region --output text)
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-pnpx aws-cdk gc aws://$ACCOUNT_ID/$REGION --unstable gc --confirm true
+cdk gc aws://$ACCOUNT_ID/$REGION --unstable gc --confirm true
 
